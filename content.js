f29bde0fc5ff528a57909e4f1a17334b553d77ea
kjirou <sorenariblog@gmail.com>
2022-04-04 23:26:45 +0900

    Write logic

--- content.js
+++ content.js
@@ -1,1 +1,7 @@
-alert('Hello, world!');
+navigator.clipboard.readText()
+  .then(async (clipText) => {
+    const formattedText = clipText
+      .replaceAll(/(\r\n|\r|\n)/g, '')
+      .replaceAll(/\. */g, '.\n')
+    return navigator.clipboard.writeText(formattedText)
+  })
