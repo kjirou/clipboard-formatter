e80ea98d17ffc0b5be760f95c8bfa1a323305f24
kjirou <sorenariblog@gmail.com>
2022-04-06 00:15:46 +0900

    Fix a bug

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 navigator.clipboard.readText()
   .then(async (clipText) => {
     const formattedText = clipText
-      .replaceAll(/(\r\n|\r|\n)/g, '')
+      .replaceAll(/(\r\n|\r|\n)/g, ' ')
       .replaceAll(/\. */g, '.\n')
     return navigator.clipboard.writeText(formattedText)
   })
